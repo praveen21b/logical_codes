9b5d72dd3283d6a4fae6f39553f8f2150b0c858f
praveen21b <praveen.h397@gmail.com>
2022-10-02 14:02:30 +0200

    fibonacci series printing on the command line

--- fibonacci.py
+++ fibonacci.py
@@ -5,11 +5,24 @@
 #         return 1
 #     return fib(n-1)+fib(n-2)
 
-def fib(n):
-    fib_ser = [0,1]
-    for i in range(n-2):
-        if i < len(fib_ser):
-            fib_ser.append(fib_ser[-1]+fib_ser[-2])
-    return fib_ser
+# print(fib(5))
 
-print(fib(5))
+# def fib(n):
+#     fib_ser = [0,1]
+#     for i in range(n-2):
+#         if i < len(fib_ser):
+#             fib_ser.append(fib_ser[-1]+fib_ser[-2])
+#     return fib_ser
+
+# print(fib(5))
+
+
+def fib(n):
+    f1,f2 = 0,1
+    for i in range(n):
+        f3 = f1+f2
+        print(f1)
+        f1 = f2
+        f2 = f3
+
+(fib(8))
