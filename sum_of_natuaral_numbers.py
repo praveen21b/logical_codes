66674af765b8019748d645c759c3295e0be76c8e
praveen21b <praveen.h397@gmail.com>
2022-10-02 12:46:49 +0200

    sum_of_natural_numbars_formula1

--- sum_of_natuaral_numbers.py
+++ sum_of_natuaral_numbers.py
@@ -1,7 +1,10 @@
+# def sum_of_natural_numbars(target):
+#     total = 0
+#     for num in range(1,target+1):
+#         total += num
+#     return total
+
 def sum_of_natural_numbars(target):
-    total = 0
-    for num in range(1,target+1):
-        total += num
-    return total
+    return (target/2)*(1+target)
 
 print(sum_of_natural_numbars(6))
